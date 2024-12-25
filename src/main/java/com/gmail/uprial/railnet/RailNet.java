f4b1795769e48f2da6f1d4e5c7cc77b73a15c120
Ilya Roslyakov <i_roslyakov@wargaming.net>
2024-12-25 20:53:25 +0200

    Removed cron batching

--- src/main/java/com/gmail/uprial/railnet/RailNet.java
+++ src/main/java/com/gmail/uprial/railnet/RailNet.java
@@ -14,7 +14,6 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitTask;
 
 import java.io.File;
 
--- src/main/java/com/gmail/uprial/railnet/RailNetCron.java
+++ src/main/java/com/gmail/uprial/railnet/RailNetCron.java
@@ -1,14 +1,12 @@
 package com.gmail.uprial.railnet;
 
 import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.scheduler.BukkitTask;
 
 import java.util.Queue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class RailNetCron extends BukkitRunnable {
     private static final int INTERVAL = 1;
-    private static final int BATCH_SIZE = 100;
 
     private static final Queue<Runnable> DEFERRED_TASKS = new LinkedBlockingQueue<>();
     private static final Queue<Runnable> ACTIVE_TASKS = new LinkedBlockingQueue<>();
@@ -37,10 +35,8 @@ public class RailNetCron extends BukkitRunnable {
             task.run();
         }
 
-        int activated = 0;
-        while(((task = DEFERRED_TASKS.poll()) != null) && (activated < BATCH_SIZE)) {
+        while((task = DEFERRED_TASKS.poll()) != null) {
             ACTIVE_TASKS.add(task);
-            activated++;
         }
     }
 }
